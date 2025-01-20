5ec0097a1260662f747d03e6fc15cdb2a0ab8aa0
Hammid-more <lawhammed45@gmail.com>
2025-01-20 21:15:28 +0100

    lesson2.c

---
+++ lesson2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 10;
+
+	//Expression - Anything that can be evaluated (to 1 value)
+	//statement
+	//declaration
+	//initialization
+	
+	int y = x / 2;
+
+	printf("Hello world!\n")
+	return 0;
+} 
